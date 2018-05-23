29a2db8408089814d990d3e777361b666384a3d2
Alex Winitzky <alex.winitzky@gmail.com>
2018-05-22 18:47:09 -0600

    add and delete functionality

--- routes/flat.js
+++ routes/flat.js
@@ -2,29 +2,43 @@ var express = require('express');
 var router = express.Router();
 var Flatearth = require('../models').Flatearth;
 
-var evidence = [
-  {id: 1, fact: "The world doesn't LOOK round."},
-  {id: 2, fact: "Scientists are liars."},
-  {id: 3, fact: "Obama."}
-]
+// var evidence = [
+//   {id: 1, evidence: "The world doesn't LOOK round."},
+//   {id: 2, evidence: "Scientists are liars."},
+//   {id: 3, evidence: "Obama."}
+// ]
+
 /* GET evidence. */
 router.get('/', function(req, res) {
-  res.render('flat', {fact: evidence});
+  Flatearth.all()
+  .then(function (evidence) {
+    res.render('flat', {fact: evidence});
+  })
 });
+
+/* POST /evidence */
+router.post('/', function (req, res) {
+  var fact = req.body.evidence
+  Flatearth.create({ evidence: fact })
+    .then(function () {
+      res.redirect('/flat')
+    })
+})
+
+/* DELETE /evidence/ */
+router.delete('/:id', function (req, res) {
+  Flatearth.findById(req.params.id)
+    .then(function (fact) {
+      fact.destroy()
+      return res.redirect('/flat')
+    })
+})
 
 router.get('/form', function(req, res) {
   res.render('form');
 });
 
 
-/* POST /evidence */
-router.post('/', function (req, res) {
-  var fact = req.body.fact
-  Flatearth.create({ fact: fact })
-    .then(function () {
-      res.redirect('/evidence')
-    })
-})
 
 
 module.exports = router;
